76252581b115dfea3960c4c7aad30848c3247059
Amrit Shrestha <shresthaamrit979@gmail.com>
2025-05-16 00:53:36 +0545

    display our products into home page

--- category/models.py
+++ category/models.py
@@ -15,3 +15,4 @@ class Category(models.Model):
  
     def __str__(self):
         return self.category_name
+    
--- greatkart/views.py
+++ greatkart/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render  #import render function from django.shortcuts module
+from store.models import Product
 
 def home(request):  #function to handle the home page
-    return render(request, 'home.html') #render the home.html template
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'home.html', context) #render the home.html template
+
